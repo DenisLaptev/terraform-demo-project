4824056cfad030f9b122c0be1c22166b07c556b8
DenysLaptiev <laptiev.denys.18@gmail.com>
2023-09-22 16:13:15 +0300

    use vpc module from tf registry

--- main.tf
+++ main.tf
@@ -5,34 +5,31 @@ provider "aws" {
   secret_key = ""
 }
 
-resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.vpc_cidr_block
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = var.vpc_cidr_block
+
+  azs                = [var.avail_zone]
+  public_subnets     = [var.subnet_cidr_block]
+  public_subnet_tags = { Name = "${var.env_prefix}-subnet-1" }
+
   tags = {
     Name = "${var.env_prefix}-vpc"
   }
-}
-
-module "myapp-subnet" {
-  source = "./modules/subnet"
-
-  #setting variables
-  subnet_cidr_block      = var.subnet_cidr_block
-  avail_zone             = var.avail_zone
-  env_prefix             = var.env_prefix
-  vpc_id                 = aws_vpc.myapp-vpc.id
-  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
 
 module "myapp-server" {
   source = "./modules/webserver"
 
   #setting variables
-  vpc_id              = aws_vpc.myapp-vpc.id
+  vpc_id              = module.vpc.vpc_id
   my_ip_cidr          = var.my_ip_cidr
   env_prefix          = var.env_prefix
   image_id            = var.image_id
   public_key_location = var.public_key_location
   instance_type       = var.instance_type
-  subnet_id           = module.myapp-subnet.subnet.id
+  subnet_id           = module.vpc.public_subnets[0]
   avail_zone          = var.avail_zone
 }
